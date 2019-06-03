2a27952a9665fd35eade7acf88338604224f36d5
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-06-03 09:17:45 +0500

    Initial commit

---
+++ src/main/java/com/stegnin/miecosystem/MiEcosystemApplication.java
@@ -0,0 +1,13 @@
+package com.stegnin.miecosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiEcosystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiEcosystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stegnin/miecosystem/MiEcosystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stegnin.miecosystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiEcosystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
