7e67631b5447de1f3e1904b228b56f7c26825e33
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-06-03 09:23:37 +0500

    Добавлен класс "Информация об устройстве"

---
+++ src/main/java/com/stegnin/miecosystem/model/DeviceInfo.java
@@ -0,0 +1,14 @@
+package com.stegnin.miecosystem.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class DeviceInfo extends Device {
+
+}
